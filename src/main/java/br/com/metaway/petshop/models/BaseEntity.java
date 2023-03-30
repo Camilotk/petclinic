3874a4866b65644ab2f16320aab25b0b5ac561b8
Camilo Cunha de Azevedo <camilotk@gmail.com>
2023-03-30 20:54:07 -0300

    :recycle: Rename Entity class to not conflict with Jakarta

--- src/main/java/br/com/metaway/petshop/models/BaseEntity.java
+++ src/main/java/br/com/metaway/petshop/models/BaseEntity.java
@@ -14,7 +14,7 @@ import jakarta.persistence.MappedSuperclass;
  *
  */
 @MappedSuperclass
-public class Entity {
+public class BaseEntity {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	protected BigInteger id;
