58d0bef6fb4403bc92a251b9d7e00686cdacb303
Camilo Cunha de Azevedo <camilotk@gmail.com>
2023-03-30 19:31:01 -0300

    :construction: Add endpoint for update a Race

--- src/main/java/br/com/metaway/petshop/controllers/RaceController.java
+++ src/main/java/br/com/metaway/petshop/controllers/RaceController.java
@@ -10,6 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -45,5 +46,18 @@ public class RaceController {
 	        return ResponseEntity.notFound().build();
 	    }
 	}
-
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<Race> update(@PathVariable BigInteger id, @RequestBody Race updatedRace) {
+	    Optional<Race> race = this.repository.findById(id);
+	    if (race.isPresent()) {
+	        Race existingRace = race.get();
+	        existingRace.setDescription(updatedRace.getDescription());
+	        Race savedRace = this.repository.save(existingRace);
+	        return ResponseEntity.ok(savedRace);
+	    } else {
+	        return ResponseEntity.notFound().build();
+	    }
+	}
+	
 }
