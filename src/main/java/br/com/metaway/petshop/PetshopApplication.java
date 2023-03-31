4927626f0f327fe7a9524618fb99fb12b68793f9
Camilo Cunha de Azevedo <camilotk@gmail.com>
2023-03-30 21:43:47 -0300

    :fire: Remove unused annotation

--- src/main/java/br/com/metaway/petshop/PetshopApplication.java
+++ src/main/java/br/com/metaway/petshop/PetshopApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-// @ComponentScan(basePackages = {"br.com.metaway.petshop"})
 public class PetshopApplication {
 
 	public static void main(String[] args) {
