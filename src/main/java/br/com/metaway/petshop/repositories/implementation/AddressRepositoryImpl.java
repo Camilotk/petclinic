997e8c6840e40f52fabad2a7ed503f360c158c67
Camilo Cunha de Azevedo <camilotk@gmail.com>
2023-03-30 21:44:44 -0300

    :construction: Update the Impl from AddressRepository

--- src/main/java/br/com/metaway/petshop/repositories/implementation/AddressRepositoryImpl.java
+++ src/main/java/br/com/metaway/petshop/repositories/implementation/AddressRepositoryImpl.java
@@ -1,17 +1,25 @@
 package br.com.metaway.petshop.repositories.implementation;
 
 import java.math.BigInteger;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+import org.aspectj.lang.annotation.SuppressAjWarnings;
 import org.springframework.dao.DataAccessException;
+import org.springframework.stereotype.Repository;
 
 import br.com.metaway.petshop.models.Address;
 import br.com.metaway.petshop.repositories.AddressRepository;
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.transaction.Transactional;
 
+@Repository
+@Transactional
 public class AddressRepositoryImpl implements AddressRepository {
 	
+	@PersistenceContext
 	private EntityManager em;
 
 	@Override
@@ -19,11 +27,18 @@ public class AddressRepositoryImpl implements AddressRepository {
 		return Optional.ofNullable(this.em.find(Address.class, id));
 	}
 
+	@SuppressAjWarnings("unchecked")
 	@Override
-	public List<Address> findAll() throws DataAccessException {
-		// TODO Auto-generated method stub
-		return null;
+	public List<Address> findAll() {
+	    List<Address> races = new ArrayList<>();
+	    try {
+	        races = this.em.createQuery("FROM Address", Address.class).getResultList();
+	    } catch (Exception ex) {
+	        ex.printStackTrace();
+	    }
+	    return races;
 	}
+
 
 	@Override
 	public Address save(Address address) throws DataAccessException {
@@ -34,8 +49,7 @@ public class AddressRepositoryImpl implements AddressRepository {
 
 	@Override
 	public void delete(Address address) throws DataAccessException {
-		// TODO Auto-generated method stub
-
+		this.em.remove(em.contains(address) ? address : em.merge(address));
 	}
 
 }
