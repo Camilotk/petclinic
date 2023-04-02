9a8b7fb83711983c95c43016e410876dc7fd60f8
Camilo Cunha de Azevedo <camilotk@gmail.com>
2023-04-02 03:09:08 -0300

    :construction: Add Request for Register

--- src/main/java/br/com/metaway/petshop/web/RegisterRequest.java
+++ src/main/java/br/com/metaway/petshop/web/RegisterRequest.java
@@ -1,5 +1,17 @@
 package br.com.metaway.petshop.web;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class RegisterRequest {
-	// TODO: Make request object
+	private String firstname;
+	private String lastname;
+	private String email;
+	private String password;
 }
