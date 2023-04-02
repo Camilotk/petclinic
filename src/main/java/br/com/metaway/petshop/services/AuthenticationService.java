fc9f7e2e240a762f8c9caf902b3b59367f95fe0b
Camilo Cunha de Azevedo <camilotk@gmail.com>
2023-04-02 03:15:38 -0300

    :construction: Start implementation of Auth Service from mock

--- src/main/java/br/com/metaway/petshop/services/AuthenticationService.java
+++ src/main/java/br/com/metaway/petshop/services/AuthenticationService.java
@@ -1,8 +1,19 @@
 package br.com.metaway.petshop.services;
 
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import br.com.metaway.petshop.repositories.UserRepository;
+import lombok.RequiredArgsConstructor;
+
 @Service
+@RequiredArgsConstructor
 public class AuthenticationService {
-	// TOOD: Authentication Service
+	// properties
+	private final UserRepository repository;
+	private final PasswordEncoder passEncoder;
+	private final JwtService jwtService;
+	private final AuthenticationManager manager;
+	
 }
