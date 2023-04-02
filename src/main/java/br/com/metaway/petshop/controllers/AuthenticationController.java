b1837e67091e8de65c322f25a77e94bf43e848de
Camilo Cunha de Azevedo <camilotk@gmail.com>
2023-04-02 02:58:31 -0300

    :construction: Add mock of the first register endpoint

--- src/main/java/br/com/metaway/petshop/controllers/AuthenticationController.java
+++ src/main/java/br/com/metaway/petshop/controllers/AuthenticationController.java
@@ -1,9 +1,14 @@
 package br.com.metaway.petshop.controllers;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.metaway.petshop.services.AuthenticationService;
+import br.com.metaway.petshop.web.AuthenticationResponse;
+import br.com.metaway.petshop.web.RegisterRequest;
 import lombok.RequiredArgsConstructor;
 
 @RestController
@@ -12,6 +17,12 @@ import lombok.RequiredArgsConstructor;
 public class AuthenticationController {
 	
 	public final AuthenticationService service;
+	
+	@PostMapping("/register")
+	public ResponseEntity<AuthenticationResponse> register(
+			 @RequestBody RegisterRequest request) {
+		return ResponseEntity.ok(service);
+	}
 	
 	
 }
