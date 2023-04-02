17ee97d478b65cfbd1654f16840bf0b7666114f2
Camilo Cunha de Azevedo <camilotk@gmail.com>
2023-04-02 03:38:45 -0300

    :construction: Implement Auth Service

--- src/main/java/br/com/metaway/petshop/services/AuthenticationService.java
+++ src/main/java/br/com/metaway/petshop/services/AuthenticationService.java
@@ -1,9 +1,12 @@
 package br.com.metaway.petshop.services;
 
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import br.com.metaway.petshop.models.Role;
+import br.com.metaway.petshop.models.User;
 import br.com.metaway.petshop.repositories.UserRepository;
 import br.com.metaway.petshop.web.AuthenticationRequest;
 import br.com.metaway.petshop.web.AuthenticationResponse;
@@ -20,13 +23,26 @@ public class AuthenticationService {
 	private final AuthenticationManager manager;
 	
 	public AuthenticationResponse register(RegisterRequest request) {
-		// TODO Auto-generated method stub
-		return null;
+		User user = User.builder()
+						.firstname(request.getFirstname())
+						.lastname(request.getLastname())
+						.email(request.getEmail())
+						.password(passEncoder.encode(request.getPassword()))
+						.role(Role.USER)
+						.build();
+		 repository.save(user);
+		 String jwtToken = jwtService.generateToken(user);
+		 
+		 return AuthenticationResponse.builder().token(jwtToken).build();
 	}
 
 	public AuthenticationResponse authenticate(AuthenticationRequest request) {
-		// TODO Auto-generated method stub
-		return null;
+		manager.authenticate(new UsernamePasswordAuthenticationToken(request.getEmail(), request.getPassword()));
+		User user = repository.findByEmail(request.getEmail()).orElseThrow();
+		
+		String jwtToken = jwtService.generateToken(user);
+		 
+		return AuthenticationResponse.builder().token(jwtToken).build();
 	}
 	
 }
