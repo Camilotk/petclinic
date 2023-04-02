1a0c10bfaca5502cbd1afdadb160e45cebdf8fea
Camilo Cunha de Azevedo <camilotk@gmail.com>
2023-04-02 01:28:41 -0300

    :construction: Starting the JWT Auth Filter for security

---
+++ src/main/java/br/com/metaway/petshop/config/JwtAuthenticationFilter.java
@@ -0,0 +1,42 @@
+package br.com.metaway.petshop.config;
+
+import java.io.IOException;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+	// Execute Filter
+	@Override
+	protected void doFilterInternal(
+			@NonNull HttpServletRequest request, 
+			@NonNull HttpServletResponse response, 
+			@NonNull FilterChain filterChain)
+			throws ServletException, IOException {
+		// Constants
+		final String authHeader = request.getHeader("Authorization");
+		final String jwt;
+		final String userEmail;
+		
+		// If authHeader not exist OR authHeader is not a JWT return error
+		if (authHeader == null ||!authHeader.startsWith("Bearer ")) {
+			filterChain.doFilter(request, response);
+		    return;
+		}
+		
+		// get Header JWT info skipping "Bearer "
+		jwt = authHeader.substring(7);
+		
+	}
+
+}
