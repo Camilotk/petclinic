939a31e58c83dda3ff45ed5dfe60d5ecc7cbcc86
Camilo Cunha de Azevedo <camilotk@gmail.com>
2023-04-02 01:22:14 -0300

    :construction: Add method to Fetch user by Email

--- src/main/java/br/com/metaway/petshop/repositories/UserRepository.java
+++ src/main/java/br/com/metaway/petshop/repositories/UserRepository.java
@@ -1,11 +1,12 @@
 package br.com.metaway.petshop.repositories;
 
 import java.math.BigInteger;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.metaway.petshop.models.User;
 
 public interface UserRepository extends JpaRepository<User, BigInteger>{
-
+	Optional<User> findByEmail(String email);
 }
