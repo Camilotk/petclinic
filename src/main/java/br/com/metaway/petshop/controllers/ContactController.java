87db0d81fe3cfbef7020c2c6aa0ebb9a7a144663
Camilo Cunha de Azevedo <camilotk@gmail.com>
2023-04-04 05:57:17 -0300

    :construction: Finish the Contact Controller

--- src/main/java/br/com/metaway/petshop/controllers/ContactController.java
+++ src/main/java/br/com/metaway/petshop/controllers/ContactController.java
@@ -1,7 +1,6 @@
 package br.com.metaway.petshop.controllers;
 
 import java.math.BigInteger;
-import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
@@ -11,9 +10,11 @@ import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -90,6 +91,49 @@ public class ContactController {
 	    } else {
 	        return ResponseEntity.notFound().build();
 	    }
+	}
+
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> deleteContactById(@PathVariable BigInteger id) {
+		Optional<Contact> optionalContact = repository.findById(id);
+		if (optionalContact.isPresent()) {
+			repository.delete(optionalContact.get());
+			return ResponseEntity.noContent().build();
+		} else {
+			return ResponseEntity.notFound().build();
+		}
+	}
+
+	@PutMapping("/{id}")
+	public ResponseEntity<Map<String, Object>> updateContactById(@PathVariable BigInteger id, @RequestBody Contact updatedContact) {
+		Optional<Contact> optionalContact = repository.findById(id);
+		if (optionalContact.isPresent()) {
+			Contact contact = optionalContact.get();
+			Client currentClient = contact.getClient();
+			String currentCpf = currentClient.getCpf();
+			String updatedCpf = updatedContact.getClient().getCpf();
+			if (!currentCpf.equals(updatedCpf)) {
+				Optional<Client> optionalClient = clients.findById(updatedCpf);
+				if (optionalClient.isEmpty()) {
+					return ResponseEntity.badRequest().build();
+				}
+				Client updatedClient = optionalClient.get();
+				contact.setClient(updatedClient);
+			}
+			contact.setType(updatedContact.getType());
+			contact.setValue(updatedContact.getValue());
+			contact.setCurrency(updatedContact.getCurrency());
+			Contact updated = repository.save(contact);
+			Map<String, Object> responseBody = new LinkedHashMap<>();
+			responseBody.put("id", updated.getId());
+			responseBody.put("client_cpf", updated.getClient().getCpf());
+			responseBody.put("type", updated.getType());
+			responseBody.put("value", updated.getValue());
+			responseBody.put("currency", updated.getCurrency());
+			return ResponseEntity.ok(responseBody);
+		} else {
+			return ResponseEntity.notFound().build();
+		}
 	}
 
 
