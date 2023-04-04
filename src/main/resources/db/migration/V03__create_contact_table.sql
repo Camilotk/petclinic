8c78e260aabc48c9e8e0f8703c308ecd9f2f492d
Camilo Cunha de Azevedo <camilotk@gmail.com>
2023-04-04 03:16:29 -0300

    :card_file_box: Update DB constraints

--- src/main/resources/db/migration/V03__create_contact_table.sql
+++ src/main/resources/db/migration/V03__create_contact_table.sql
@@ -1,8 +1,8 @@
 CREATE TABLE contact (
     id SERIAL PRIMARY KEY,
-    client_cpf VARCHAR(11) NOT NULL REFERENCES client(cpf),
+    client_cpf VARCHAR(14) NOT NULL REFERENCES client(cpf) ON UPDATE CASCADE,
     type VARCHAR(255),
     price_amount NUMERIC(19,2),
     price_currency VARCHAR(3),
-    CONSTRAINT contact_client_fk FOREIGN KEY (client_cpf) REFERENCES client(cpf)
+    CONSTRAINT contact_client_fk FOREIGN KEY (client_cpf) REFERENCES client(cpf) ON UPDATE CASCADE
 );
