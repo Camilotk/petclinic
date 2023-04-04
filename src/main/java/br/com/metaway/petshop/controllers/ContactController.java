e54a5ac352f91b2c7fad3bff7c08f6494db15d35
Camilo Cunha de Azevedo <camilotk@gmail.com>
2023-04-04 00:57:07 -0300

    :construction: Add getAll endpoint for Contacts

--- src/main/java/br/com/metaway/petshop/controllers/ContactController.java
+++ src/main/java/br/com/metaway/petshop/controllers/ContactController.java
@@ -1,8 +1,11 @@
 package br.com.metaway.petshop.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,6 +25,12 @@ public class ContactController {
     public ResponseEntity<Contact> create(@RequestBody Contact contact) {
         Contact newContact = repository.save(contact);
 		return ResponseEntity.status(HttpStatus.CREATED).body(newContact);
+    }
+
+	@GetMapping
+    public ResponseEntity<List<Contact>> getAll() {
+        List<Contact> contacts = repository.findAll();
+        return ResponseEntity.ok(contacts);
     }
 
 }
