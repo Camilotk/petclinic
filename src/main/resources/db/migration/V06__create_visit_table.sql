38d39a8fd1e2849807b8f71c7060c9d7ad23dbb4
Camilo Cunha de Azevedo <camilotk@gmail.com>
2023-04-05 19:30:58 -0300

    :card_file_box: Add Visit Migration

---
+++ src/main/resources/db/migration/V06__create_visit_table.sql
@@ -0,0 +1,8 @@
+CREATE TABLE visit (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    visit_date DATE,
+    description VARCHAR(255) NOT NULL,
+    pet_id BIGINT REFERENCES pet(id) ON DELETE CASCADE,
+    price_amount NUMERIC(19,0) NOT NULL,
+    price_currency VARCHAR(3) DEFAULT 'BRL'
+);
