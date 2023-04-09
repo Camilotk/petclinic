26e89d5866ea8330ad827896c847d39133e831d9
Camilo Cunha de Azevedo <camilotk@gmail.com>
2023-04-09 03:22:01 -0300

    Add ADM Seed

---
+++ src/main/resources/db/migration/V08__insert_into_client_data.sql
@@ -0,0 +1,2 @@
+INSERT INTO client (cpf, first_name, last_name, email, password, role, registration_date) 
+VALUES ('611.857.530-44', 'John', 'Doe', 'john.doe@example.com', '$2a$10$t1qJkBhSZciIDWj75.uGPuKSus6IkklLv0ain7Md1X.uU7UrWPGma', 'ADMIN', NOW());
