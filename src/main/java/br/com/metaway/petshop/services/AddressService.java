42d08e4f2af702fad62fcb0915df8856fa16a957
Camilo Cunha de Azevedo <camilotk@gmail.com>
2023-04-10 22:02:44 -0300

    :bug: Add missing Service annotation

--- src/main/java/br/com/metaway/petshop/services/AddressService.java
+++ src/main/java/br/com/metaway/petshop/services/AddressService.java
@@ -5,11 +5,12 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
 
 import br.com.metaway.petshop.models.Address;
 import br.com.metaway.petshop.repositories.AddressRepository;
 
+@Service
 public class AddressService {
 	@Autowired
 	private AddressRepository repository;
