7748b208596f4431019bd7b050c4398db66df352
Camilo Cunha de Azevedo <camilotk@gmail.com>
2023-04-10 22:19:47 -0300

    :art: Change the JSON keys to camelCase

--- src/main/java/br/com/metaway/petshop/repositories/dtos/PetData.java
+++ src/main/java/br/com/metaway/petshop/repositories/dtos/PetData.java
@@ -3,6 +3,6 @@ package br.com.metaway.petshop.repositories.dtos;
 import java.math.BigInteger;
 import java.time.LocalDate;
 
-public record PetData(BigInteger id, String name, String client_cpf, LocalDate birth_date, BigInteger race_id) {
+public record PetData(BigInteger id, String name, String clientCpf, LocalDate birthDate, BigInteger raceId) {
 
 }
