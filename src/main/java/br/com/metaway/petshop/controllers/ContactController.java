0e32cf91841fd8d1f0f0b99b91cde833a9d365fb
Camilo Cunha de Azevedo <camilotk@gmail.com>
2023-04-15 13:57:55 -0300

    :recycle: Refactor Contact Controller to use Service

--- src/main/java/br/com/metaway/petshop/controllers/ContactController.java
+++ src/main/java/br/com/metaway/petshop/controllers/ContactController.java
@@ -23,117 +23,65 @@ import br.com.metaway.petshop.models.Client;
 import br.com.metaway.petshop.models.Contact;
 import br.com.metaway.petshop.repositories.ClientRepository;
 import br.com.metaway.petshop.repositories.ContactRepository;
+import br.com.metaway.petshop.repositories.dtos.ContactData;
+import br.com.metaway.petshop.services.ContactService;
 
 @RestController
 @RequestMapping("/contacts")
 public class ContactController {
 	
 	@Autowired
-	private ContactRepository repository;
-
-	@Autowired
-	private ClientRepository clients;
+	private ContactService service;
+	
+	@GetMapping
+	public ResponseEntity<List<ContactData>> index() {
+	    List<ContactData> contacts = service.getAll();
+	    return ResponseEntity.ok(contacts);
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<ContactData> show(@PathVariable BigInteger id) {
+	    ContactData contact = service.getById(id);
+	    
+	    if (contact == null) {
+	    	return ResponseEntity.notFound().build();
+	    }
+	    
+	    return ResponseEntity.ok(contact);
+	}
 	
 	@PostMapping
-	public ResponseEntity<Map<String, Object>> create(@RequestBody Contact contact) {
-	    String cpf = contact.getClient().getCpf();
-	    Optional<Client> optionalClient = clients.findByCpf(cpf);
-	
-	    if (optionalClient.isEmpty()) {
-	        return ResponseEntity.badRequest().build();
-	    }
-	
-	    Client client = optionalClient.get();
-	    contact.setClient(client);
-	
-	    Contact newContact = repository.save(contact);
-
-		Map<String, Object> responseBody = new LinkedHashMap<>();
-	    responseBody.put("id", newContact.getId());
-	    responseBody.put("client_cpf", newContact.getClient().getCpf());
-	    responseBody.put("type", newContact.getType());
-	    responseBody.put("value", newContact.getValue());
-	    responseBody.put("currency", newContact.getCurrency());
+	public ResponseEntity<ContactData> store(@RequestBody Contact contact) {
 	    
-	    return ResponseEntity.status(HttpStatus.CREATED).body(responseBody);
+		ContactData savedContact = service.create(contact);
+		
+		if (savedContact == null) {
+			return ResponseEntity.badRequest().build();
+		}
+	    
+	    return ResponseEntity.status(HttpStatus.CREATED).body(savedContact);
+	}
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<ContactData> update(@PathVariable BigInteger id, @RequestBody Contact contact) {
+		ContactData updatedContact = service.edit(id, contact);
+		
+		if (updatedContact == null) {
+			return ResponseEntity.notFound().build();
+		}
+		
+		return  ResponseEntity.ok(updatedContact);
 	}
 
-	@GetMapping
-	public ResponseEntity<List<Map<String, Object>>> getContacts() {
-	    List<Contact> contacts = repository.findAll();
-	    List<Map<String, Object>> responseBody = contacts.stream()
-	        .map(contact -> {
-	            Map<String, Object> contactInfo = new LinkedHashMap<>();
-	            contactInfo.put("id", contact.getId());
-	            contactInfo.put("client_cpf", contact.getClient().getCpf());
-	            contactInfo.put("type", contact.getType());
-	            contactInfo.put("value", contact.getValue());
-	            contactInfo.put("currency", contact.getCurrency());
-	            return contactInfo;
-	        })
-	        .collect(Collectors.toList());
-	    return ResponseEntity.ok(responseBody);
-	}
-
-	@GetMapping("/{id}")
-	public ResponseEntity<Map<String, Object>> getContactById(@PathVariable BigInteger id) {
-	    Optional<Contact> optionalContact = repository.findById(id);
-	    if (optionalContact.isPresent()) {
-	        Contact contact = optionalContact.get();
-	        Map<String, Object> responseBody = new LinkedHashMap<>();
-	        responseBody.put("id", contact.getId());
-	        responseBody.put("client_cpf", contact.getClient().getCpf());
-	        responseBody.put("type", contact.getType());
-	        responseBody.put("value", contact.getValue());
-	        responseBody.put("currency", contact.getCurrency());
-	        return ResponseEntity.ok(responseBody);
-	    } else {
-	        return ResponseEntity.notFound().build();
-	    }
-	}
 
 	@DeleteMapping("/{id}")
-	public ResponseEntity<Void> deleteContactById(@PathVariable BigInteger id) {
-		Optional<Contact> optionalContact = repository.findById(id);
-		if (optionalContact.isPresent()) {
-			repository.delete(optionalContact.get());
-			return ResponseEntity.noContent().build();
-		} else {
+	public ResponseEntity<Void> destroy(@PathVariable BigInteger id) {	
+		Contact deletedContact = service.deleteById(id);
+		
+		if (deletedContact == null) {
 			return ResponseEntity.notFound().build();
 		}
+		
+		return ResponseEntity.noContent().build();
 	}
-
-	@PutMapping("/{id}")
-	public ResponseEntity<Map<String, Object>> updateContactById(@PathVariable BigInteger id, @RequestBody Contact updatedContact) {
-		Optional<Contact> optionalContact = repository.findById(id);
-		if (optionalContact.isPresent()) {
-			Contact contact = optionalContact.get();
-			Client currentClient = contact.getClient();
-			String currentCpf = currentClient.getCpf();
-			String updatedCpf = updatedContact.getClient().getCpf();
-			if (!currentCpf.equals(updatedCpf)) {
-				Optional<Client> optionalClient = clients.findById(updatedCpf);
-				if (optionalClient.isEmpty()) {
-					return ResponseEntity.badRequest().build();
-				}
-				Client updatedClient = optionalClient.get();
-				contact.setClient(updatedClient);
-			}
-			contact.setType(updatedContact.getType());
-			contact.setValue(updatedContact.getValue());
-			contact.setCurrency(updatedContact.getCurrency());
-			Contact updated = repository.save(contact);
-			Map<String, Object> responseBody = new LinkedHashMap<>();
-			responseBody.put("id", updated.getId());
-			responseBody.put("client_cpf", updated.getClient().getCpf());
-			responseBody.put("type", updated.getType());
-			responseBody.put("value", updated.getValue());
-			responseBody.put("currency", updated.getCurrency());
-			return ResponseEntity.ok(responseBody);
-		} else {
-			return ResponseEntity.notFound().build();
-		}
-	}
-
-
 }
