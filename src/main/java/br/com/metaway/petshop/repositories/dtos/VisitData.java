7147f2a31e0b6d5ff79dcd6cf159be79c4f05a49
Camilo Cunha de Azevedo <camilotk@gmail.com>
2023-04-16 00:50:28 -0300

    :construction: Add Visit Data representation

---
+++ src/main/java/br/com/metaway/petshop/repositories/dtos/VisitData.java
@@ -0,0 +1,8 @@
+package br.com.metaway.petshop.repositories.dtos;
+
+import java.math.BigInteger;
+import java.util.Date;
+
+public record VisitData(BigInteger id, BigInteger petId, Date date, String description, BigInteger value, String currency) {
+
+}
