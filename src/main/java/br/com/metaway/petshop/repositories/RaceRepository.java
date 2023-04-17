87b09d218a63a56e35671e2c42748a686e31b95e
Camilo Cunha de Azevedo <camilotk@gmail.com>
2023-04-17 20:40:33 -0300

    :bug: Fix the mistype on the Repository

--- src/main/java/br/com/metaway/petshop/repositories/RaceRepository.java
+++ src/main/java/br/com/metaway/petshop/repositories/RaceRepository.java
@@ -10,6 +10,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import br.com.metaway.petshop.models.Pet;
 import br.com.metaway.petshop.models.Race;
 
-public interface RaceRepository extends JpaRepository<Pet, BigInteger> {
+public interface RaceRepository extends JpaRepository<Race, BigInteger> {
 
 }
