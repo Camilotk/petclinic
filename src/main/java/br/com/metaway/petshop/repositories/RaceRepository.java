7bf4ea3cc365d799f3e985c53e5ccf84c6a69984
Camilo Cunha de Azevedo <camilotk@gmail.com>
2023-04-17 20:38:55 -0300

    :recycle: Refactor the Race to use the Jpa Repository

--- src/main/java/br/com/metaway/petshop/repositories/RaceRepository.java
+++ src/main/java/br/com/metaway/petshop/repositories/RaceRepository.java
@@ -5,17 +5,11 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.dao.DataAccessException;
+import org.springframework.data.jpa.repository.JpaRepository;
 
+import br.com.metaway.petshop.models.Pet;
 import br.com.metaway.petshop.models.Race;
 
-public interface RaceRepository {
-	
-	Optional<Race> findById(BigInteger id) throws DataAccessException;
-	
-	List<Race> findAll() throws DataAccessException;
-	
-	Race save(Race race) throws DataAccessException;
-	
-	void delete(Race race) throws DataAccessException;
+public interface RaceRepository extends JpaRepository<Pet, BigInteger> {
 
 }
