7464c37a779cab1a3246028ad77ae17b07ecb625
Camilo Cunha de Azevedo <camilotk@gmail.com>
2023-04-17 23:53:25 -0300

    :recycle: Add lombok annotations to Race Model

--- src/main/java/br/com/metaway/petshop/models/Race.java
+++ src/main/java/br/com/metaway/petshop/models/Race.java
@@ -1,9 +1,17 @@
 package br.com.metaway.petshop.models;
 
 import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 public class Race extends BaseEntity {
 	@Getter @Setter
