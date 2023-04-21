932e5da5ee79cf219b62e7cdfc0920d1cb622c58
Camilo Cunha de Azevedo <camilotk@gmail.com>
2023-04-21 17:16:47 -0300

    :wrench: Add the config for Cache

--- src/main/java/br/com/metaway/petshop/PetshopApplication.java
+++ src/main/java/br/com/metaway/petshop/PetshopApplication.java
@@ -2,8 +2,10 @@ package br.com.metaway.petshop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class PetshopApplication {
 
 	public static void main(String[] args) {
