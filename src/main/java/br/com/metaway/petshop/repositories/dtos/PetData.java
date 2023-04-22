bacae9387d465d76f4e0fdbd984e3da0c87520a3
Camilo Cunha de Azevedo <camilotk@gmail.com>
2023-04-21 21:11:23 -0300

    🚧 Implements Serizable for PetData

--- src/main/java/br/com/metaway/petshop/repositories/dtos/PetData.java
+++ src/main/java/br/com/metaway/petshop/repositories/dtos/PetData.java
@@ -1,8 +1,9 @@
 package br.com.metaway.petshop.repositories.dtos;
 
+import java.io.Serializable;
 import java.math.BigInteger;
 import java.time.LocalDate;
 
-public record PetData(BigInteger id, String name, String clientCpf, LocalDate birthDate, BigInteger raceId) {
+public record PetData(BigInteger id, String name, String clientCpf, LocalDate birthDate, BigInteger raceId) implements Serializable {
 
 }
