4db98c4be80def93a16c7047f6cd4d1a98a3f593
Camilo Cunha de Azevedo <camilotk@gmail.com>
2023-04-21 21:11:12 -0300

    🚧 Implements Serizable for ContactData

--- src/main/java/br/com/metaway/petshop/repositories/dtos/ContactData.java
+++ src/main/java/br/com/metaway/petshop/repositories/dtos/ContactData.java
@@ -1,9 +1,10 @@
 package br.com.metaway.petshop.repositories.dtos;
 
+import java.io.Serializable;
 import java.math.BigInteger;
 
 import br.com.metaway.petshop.models.ContactType;
 
-public record ContactData(BigInteger id, String clientCpf, ContactType type, BigInteger value, String currency) {
+public record ContactData(BigInteger id, String clientCpf, ContactType type, BigInteger value, String currency) implements Serializable {
 
 }
