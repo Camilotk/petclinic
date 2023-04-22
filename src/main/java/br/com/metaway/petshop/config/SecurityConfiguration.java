6fbd1e31ac585c8c3416f0e9aee7becbeed707a3
Camilo Cunha de Azevedo <camilotk@gmail.com>
2023-04-22 03:52:56 -0300

    :lock: Change Security Config to allow Swagger

--- src/main/java/br/com/metaway/petshop/config/SecurityConfiguration.java
+++ src/main/java/br/com/metaway/petshop/config/SecurityConfiguration.java
@@ -8,6 +8,8 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.security.web.util.matcher.OrRequestMatcher;
 
 import br.com.metaway.petshop.filters.JwtAuthenticationFilter;
 import lombok.RequiredArgsConstructor;
@@ -23,7 +25,11 @@ public class SecurityConfiguration {
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
 		http.csrf().disable().authorizeHttpRequests()
-		        .requestMatchers("/auth/**").permitAll().anyRequest().authenticated().and()
+		        .requestMatchers(new OrRequestMatcher(new AntPathRequestMatcher("/"),
+		        									  new AntPathRequestMatcher("/auth/**"), 
+		        		                              new AntPathRequestMatcher("/swagger-ui/**"),
+		        		                              new AntPathRequestMatcher("/v3/api-docs/**"))).permitAll()
+		        .anyRequest().authenticated().and()
 				.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and() // not hold state
 				.authenticationProvider(authenticationProvider)
 				.addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
