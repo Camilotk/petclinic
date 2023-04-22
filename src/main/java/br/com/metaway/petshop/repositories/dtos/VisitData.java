4d9449ee2726a3a5ab8e9b2dd79ab375d3ddcfd0
Camilo Cunha de Azevedo <camilotk@gmail.com>
2023-04-21 21:11:58 -0300

    🚧 Implements Serializable for VisitData

--- src/main/java/br/com/metaway/petshop/repositories/dtos/VisitData.java
+++ src/main/java/br/com/metaway/petshop/repositories/dtos/VisitData.java
@@ -1,8 +1,9 @@
 package br.com.metaway.petshop.repositories.dtos;
 
+import java.io.Serializable;
 import java.math.BigInteger;
 import java.util.Date;
 
-public record VisitData(BigInteger id, BigInteger petId, Date date, String description, BigInteger value, String currency) {
+public record VisitData(BigInteger id, BigInteger petId, Date date, String description, BigInteger value, String currency) implements Serializable {
 
 }
